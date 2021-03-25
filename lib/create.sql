dbd1e6c8da7ed36a52907f3cb26a88518ec600c1
Jouse then <jthen93@gmail.com>
2021-03-25 19:37:42 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -2,3 +2,15 @@ CREATE TABLE user (
 id INTEGER PRIMARY KEY, 
 name TEXT,
 age INTEGER);
+
+CREATE TABLE project (
+id INTEGER PRIMARY KEY, 
+title TEXT,
+category TEXT,
+funding_goal INTEGER, 
+start_date);
+
+CREATE TABLE user (
+id INTEGER PRIMARY KEY, 
+name TEXT,
+age INTEGER);
